fec0520ae62fcc07c337843724872f379879bbfd
hikidahiroyuki <hhikida513wpgk1@gmail.com>
2021-07-20 17:52:25 +0900

    210719-excutable

--- KaicoTracker.py
+++ KaicoTracker.py
@@ -119,7 +119,8 @@ def DistantCalc(data):
 
 
 def CalcAreaChange(df, length=-1):
-    _, axs = plt.subplots(nrows=1, ncols=args.segment+1, figsize=(args.segment+1, 1))
+    numSegment = (endAnalysis - initialAnalysis) // args.segment + 1
+    _, axs = plt.subplots(nrows=1, ncols=numSegment, figsize=(numSegment, 1))
     plt.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0)
     Xcen = df.XM.mean()
     Ycen = df.YM.mean()
@@ -127,9 +128,8 @@ def CalcAreaChange(df, length=-1):
         Xstd = df.XM.std()
         Ystd = df.YM.std()
         length = max(Xstd, Ystd, 1) * 3
-    for k in range(0, args.segment+1):
-        trange = (endAnalysis - initialAnalysis) // args.segment
-        part = df[(df.Slice > trange * k) & (df.Slice <= trange * (k+1))]
+    for k in range(0, numSegment):
+        part = df[(df.Slice > args.segment * k) & (df.Slice <= args.segment * (k+1))]
         axs[k].set_xlim(Xcen-length, Xcen+length)
         axs[k].set_ylim(Ycen-length, Ycen+length)
         axs[k].scatter(part.XM, part.YM, s=0.1, alpha=0.5, c='gray')
@@ -179,8 +179,7 @@ def plotDuration(dfDur, dfDist):
     for i in dfDur.index:
         activeTimePoint += list(np.arange(dfDur.loc[i, 'start'], dfDur.loc[i, 'end']))
     plotDur = [1 if i in activeTimePoint else 0 for i in listDur]
-    trange = (listDur.max() - listDur.min()) // args.segment
-    _, axs = plt.subplots(nrows=2, ncols=1, figsize=(args.segment+1, 2), gridspec_kw={'height_ratios':[3,1]})
+    _, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 1), gridspec_kw={'height_ratios':[4,1]})
     if args.format == 'png':
         plt.subplots_adjust(wspace=0, hspace=0.1)
     else:
@@ -194,8 +193,8 @@ def plotDuration(dfDur, dfDist):
     axs[1].set_xlim(listDur.min(), listDur.max())
     axs[1].set_ylim(0,1)
     axs[1].set_yticks([])
-    axs[1].set_xticks(range(0, listDur.max()-listDur.min(), trange))
-    axs[1].set_xticklabels([i*args.lapse for i in range(0, listDur.max()-listDur.min(), trange)])
+    axs[1].set_xticks(range(0, listDur.max()-listDur.min(), args.segment))
+    axs[1].set_xticklabels([i*args.lapse for i in range(0, listDur.max()-listDur.min(), args.segment)])
     sns.despine()
     plt.savefig('{}_locomotion_{}.{}'.format(args.prefix, dfDist['id'].iloc[0], args.format), format=args.format, dpi=200)
     plt.close('all')
@@ -242,7 +241,7 @@ if __name__ == '__main__':
     parser.add_argument('--bottom', metavar='px', type=int, help='bottom position of frame', default=0)
     parser.add_argument('--left', metavar='px', type=int, help='left position of frame', default=0)
     parser.add_argument('--right', metavar='px', type=int, help='right position of frame', default=-1)
-    parser.add_argument('--segment', metavar='int', type=int, help='how many segment used for area segment', default=-1)
+    parser.add_argument('--segment', metavar='int', type=int, help='the length of segment used for area segmentation', default=-1)
     parser.add_argument('--segment_edge_length', metavar='px', type=int, help='length of segment square', default=-1)
     parser.add_argument('--window', metavar='frames', type=int, help='seed window for duration analysis', default=10)
     parser.add_argument('--format', type=str, help='output format for figures', default='png', choices=['png', 'pdf'])
